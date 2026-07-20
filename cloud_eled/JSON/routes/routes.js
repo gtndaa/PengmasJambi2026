b936634c8f6fa4f7117a9f87bfada327c16afc30
galihmsa165 <167429719+galihmsa165@users.noreply.github.com>
2026-07-20 10:29:16 +0700

    Update routes.js

--- cloud_eled/JSON/routes/routes.js
+++ cloud_eled/JSON/routes/routes.js
@@ -1,50 +1,35 @@
 const express = require('express');
-const axios = require('axios');
-const { getUsers, register, login, ubahTanaman } =
-require('../controller/Users');
-const { getAllLokasiLahan, getAllLocationKey, getOneLocationKey } =
-require('../controller/LokasiLahan');
-const { getAllWeatherCondition, getLatestWeatherCondition,
-insertOneHourlyWeatherCondition, insertTwelveHourlyWeatherCondition,
-updateWeatherForecast, get12HoursForecasts } =
-require('../controller/WeatherCondition');
+const router = express.Router();
+
+// Import Controllers
+const { getUsers, register, login, ubahTanaman } = require('../controller/Users');
+const { getAllLokasiLahan, getAllLocationKey, getOneLocationKey } = require('../controller/LokasiLahan');
 const { getAllKegiatan } = require('../controller/Kalender');
 const { authenticateJWT } = require('../middleware/tokenVerification');
-const router = express.Router();
-const middle = express.urlencoded({ extended: false });
-router.get('/', async (req, res) => {
- res.status(200).json({
- message: "Welcome to Agrical!"
- })
+
+// (Nanti tambahkan ini setelah Anda buat SensorController)
+// const { saveSensorData, getLatestSensorData } = require('../controller/SensorController');
+
+// Home Route
+router.get('/', (req, res) => {
+    res.status(200).json({ message: "Welcome to Agrical Backend!" });
 });
-// Route untuk user (signup, signin, settings, users, delete, lokasilahan)
+
+// Auth & User Routes
 router.get('/protected', authenticateJWT, (req, res) => {
- res.json({ message: `Welcome, ${req.user.username}` });
+    res.json({ message: `Welcome, ${req.user.username}` });
 });
 router.get('/users', getUsers);
 router.post('/register', register);
 router.post('/login', login);
 router.post('/ubahtanaman', ubahTanaman);
-// Route untuk Dropdown Lokasi Kecamatan
+
+// Lokasi Lahan Routes
 router.get('/getalllokasilahan', getAllLokasiLahan);
-router.post('/getonelocationkey', getOneLocationKey);
-// Route untuk Profil
-// edit nama
-// edit username
-// edit email
-// change paswsword
-// change tanaman
-// log out
-// Route untuk weathercondition ()
-router.get('/allweathercondition', getAllWeatherCondition);
-router.post('/latestweathercondition', getLatestWeatherCondition);
-router.get('/addweathercondition', insertOneHourlyWeatherCondition);
-router.get('/add12weathercondition', insertTwelveHourlyWeatherCondition);
-// Route untuk weatherforecast
-router.get('/updateforecast', updateWeatherForecast);
-router.post('/displayforecasts', get12HoursForecasts);
-// Route untuk kalender
-router.get('/allkegiatan', getAllKegiatan);
-// Route untuk Lokasi Lahan
 router.get('/alllocationkey', getAllLocationKey);
+router.post('/getonelocationkey', getOneLocationKey);
+
+// Kalender Routes
+router.get('/allkegiatan', getAllKegiatan);
+
 module.exports = { router };
