380a03987b9eb127f9e2f1a2d82b932c42c6e17b
kiyoleetiono <167530852+kiyoleetiono@users.noreply.github.com>
2026-08-04 19:48:49 +0700

    Delete app_eled directory

--- app_eled/esp_lambda_test/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#define DEVICE_NAME "ESP_LAMBDA_TEST"
-
-#define BASE_URL "https://k27gamn56cmjkns7mcjny4wovu0jbems.lambda-url.ap-southeast-3.on.aws"
-
-#endif
--- app_eled/lib/consts/mqtt_topic.dart
+++
@@ -1,9 +0,0 @@
-class MQTTTopic {
-
-  static const String publishSensor =
-      "device/data";
-
-  static const String subscribeConfig =
-      "device/config";
-
-}
